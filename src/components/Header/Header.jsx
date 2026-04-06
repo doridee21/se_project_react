e94c6f3e3bc248df91e51a4f0e4fb0f145004f7d
doridee21 <dorion.miller90@gmail.com>
2026-04-06 14:04:15 -0400

    updates to project react

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../assets/logo.png";
+import avatar from "../../assets/user-avatar.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="logo" />
+      <p className="header__date-location">October 10, 2023 | New York, NY</p>
+      <button className="header__clothes-btn">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrance Tegegne</p>
+        <img className="header__avatar" src={avatar} alt="Terrance Tegegne" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
