97df7496c3864f597494297a045ec07d8b2cf5f7
doridee21 <dorion.miller90@gmail.com>
2026-04-20 16:42:22 -0400

    updates to project react

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,17 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import ItemModal from "../ItemModal/ItemModal";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "warm" });
-  const [activeModal, setActiveModal] = useState("");
+  const [activeModal, setActiveModal] = useState("preview");
+  const [selectedCard, setSelectedCard] = useState({});
+
+  const handleCardClick = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
 
   const handleAddBtnClick = () => {
     setActiveModal("new-garment");
@@ -21,13 +28,13 @@ function App() {
     <div className="page">
       <div className="page__content">
         <Header handleAddBtnClick={handleAddBtnClick} />
-        <Main weatherData={weatherData} />
+        <Main weatherData={weatherData} handleCardClick={handleCardClick} />
       </div>
       <ModalWithForm
         buttonText="Add garment"
         title="New Garment"
         activeModal={activeModal}
-        handleCloseClick={closeActiveModal}
+        onClose={closeActiveModal}
       >
         <label htmlFor="name" className="modal__label">
           Name{" "}
@@ -66,6 +73,11 @@ function App() {
           </label>
         </fieldset>
       </ModalWithForm>
+      <ItemModal
+        activeModal={activeModal}
+        card={selectedCard}
+        onClose={closeActiveModal}
+      />
     </div>
   );
 }
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,21 +1,11 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({
-  children,
-  buttonText,
-  title,
-  activeModal,
-  handleCloseClick,
-}) {
+function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
     <div className={`modal ${activeModal === "new-garment" && "modal_open"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button
-          className="modal__close-btn"
-          type="button"
-          onClick={handleCloseClick}
-        >
+        <button className="modal__close-btn" type="button" onClick={onClose}>
           ×
         </button>
         <form className="modal__form">
